3d1708f976db1afcf2435238c19decdc8132d286
Teddy Sung <teddysung168@gmail.com>
2019-07-24 21:24:12 +0800

    The sixth version count the amount of reviews using under 100 words

---
+++ R.A.6.py
@@ -0,0 +1,18 @@
+data = []
+count = 0
+with open('reviews.txt', 'r') as f:
+	for line in f:
+		data.append(line)
+		count += 1
+		if count % 1000 == 0:
+			print(len(data))
+print('算完了，共有', len(data), '筆資料')
+sum_data = 0
+for d in data:
+	sum_data += len(d)
+print('平均長度是', sum_data / len(data))
+new = []
+for d in data:
+	if len(d) < 100:
+		new.append(d)
+print('共有', len(new), '筆少於100字')
