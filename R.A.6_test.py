3b07b3c13ab6662f95ee582a430d7ae413489613
Teddy Sung <teddysung168@gmail.com>
2019-07-24 21:25:54 +0800

    It is just a fun code, it can count how many 'e' in the reviews

---
+++ R.A.6_test.py
@@ -0,0 +1,19 @@
+data = []
+count = 0
+with open('reviews.txt', 'r') as f:
+	for line in f:
+		data.append(line)
+		count += 1
+		if count % 1000 == 0:
+			print(len(data))
+print('算完了，共有', len(data), '筆資料')
+sum_data = 0
+for d in data:
+	sum_data += len(d)
+print('平均長度是', sum_data / len(data))
+new = []
+for d in data:
+	for char in d:
+		if char == 'e':
+			new.append(char)
+print(len(new))
